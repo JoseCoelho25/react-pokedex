a282d5d5eca90cef6dad98cf79e7766f1c6b62c0
JoseCoelho25 <zenani87@hotmail.com>
2023-01-21 00:00:51 +0000

    request API for homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,14 +11,15 @@ function HomePage() {
           });
       }, []);
 
-      console.log(pokemons[0])
+     
   return (
     <div>
-         <div>
-            {pokemons.map((pokemon, index)=>(
-                <ul key={index}>
-                    <li>{pokemon[0].name}</li>
-                </ul>
+         <div className="container grid grid-cols-4">
+            {pokemons.map((p, index)=>(
+                <div key={index}>
+                    <h3>{p.name}</h3>
+                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={p.name} /> 
+                </div>             
             ))}
         </div> 
     </div>
