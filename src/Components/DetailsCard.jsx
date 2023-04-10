dbf81bd7f05d652f4f39b5e4063e59ffef76e432
JoseCoelho25 <zenani87@hotmail.com>
2023-04-10 15:22:46 +0100

    poketypes bgcolor dynamic change

--- src/Components/DetailsCard.jsx
+++ src/Components/DetailsCard.jsx
@@ -9,38 +9,99 @@ import PokeEggs from './DetailsComponents/PokeEggs';
 import PokeHatch from './DetailsComponents/PokeHatch';
 import PokeDimensions from './DetailsComponents/PokeDimensions';
 import PokeNameId from './DetailsComponents/PokeNameId';
-import PokeBg from './PokeBg';
+
 
 
 function DetailsCard() {
     const { id } = useParams();
     const [pokemon, setPokemon] = useState({});
+    const [bgColor, setBgColor] = useState("");
   
+    
     useEffect(() => {
       axios
         .get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
         .then(response => {
-          // had to do the uppercase here in useeffect because of the pokemon initial rendering
           response.data.name = response.data.name.charAt(0).toUpperCase() + response.data.name.slice(1);
           setPokemon(response.data)
+          if (response.data.types) {
+            const promises = response.data.types.map((type) => axios.get(type.type.url));
+            Promise.all(promises)
+              .then((responses) => {
+                const types = responses.map((response) => response.data.name);
+                switch(types[0]){
+                  case "fire":
+                    setBgColor("bg-[#F57D31]")
+                    break;
+                  case "poison":
+                    setBgColor("bg-[#7B61FF]")
+                    break;
+                  case "water":
+                    setBgColor("bg-[#6493EB]")
+                    break;
+                  case "grass":
+                    setBgColor("bg-[#74CB48]")
+                    break;
+                  case "electric":
+                    setBgColor("bg-[#F9CF30]");
+                    break;
+                  case "ground":
+                    setBgColor("bg-[#DEC16B]");
+                    break;
+                  case "fairy":
+                    setBgColor("bg-[#E69EAC]");
+                    break;
+                  case "fighting":
+                    setBgColor("bg-[#C12239]");
+                    break;
+                  case "psychic":
+                    setBgColor("bg-[#FB5584]");
+                    break;
+                  case "dragon":
+                    setBgColor("bg-[#7037FF]");
+                    break;
+                  case "ghost":
+                    setBgColor("bg-[#70559B]");
+                    break;
+                  case "ice":
+                    setBgColor("bg-[#9AD6DF]");
+                    break;
+                  case "bug":
+                    setBgColor("bg-[#A7B723]");
+                    break;
+                  case "rock":
+                    setBgColor("bg-[#B69E31]");
+                    break;
+                  case "steel":
+                    setBgColor("bg-[#B7B9D0]");
+                    break;
+                  case "dark":
+                    setBgColor("bg-[#75574C]");
+                    break;
+                  default:
+                    setBgColor("bg-[#AAA67F]")
+                }
+              })
+              .catch((error) => console.log(error));
+          }
         })
-        .catch(error => console.log(error));
-        
+        .catch(error => console.log(error));   
     }, [id]);
-   
+    
       
     return (
-      <PokeBg pokemon={pokemon}>
-        <div className="p-5">
+      
+        <div className={`p-5 ${bgColor}`}>
           <PokeNameId pokemon={pokemon}/>
-        
-        {/* ternary operator to allow the request for the API sprites */}
-        {pokemon.sprites && pokemon.sprites.front_default ? 
-         <img src={pokemon.sprites.front_default} alt={pokemon.name} className="w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/5 mx-auto bg-white rounded-xl mt-2"/>:
+        {pokemon.sprites && pokemon.sprites.front_default ?
+        <div className={`${bgColor} w-full h-48`}>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} className={`w-full md:w-1/2 lg:w-1/3 xl:w-1/5 mx-auto bg-no-bg rounded-xl mt-2`}/>
+        </div> 
+         :
          <p>Loading...</p>}
-         <div className="flex w-1/2 mx-auto text-start text-2xl mt-5 mb-5">
-            <div className="mr-2">Type:</div>
-            <PokeTypes pokemon={pokemon} />
+         <div className='bg-white w-full h-fit rounded-lg'>
+          <div className="flex w-1/2 mx-auto text-start text-2xl pt-24 mb-5">
+            <PokeTypes pokemon={pokemon} bgColor={bgColor}/>
          </div>
   
           <PokeAbility pokemon={pokemon} />
@@ -57,9 +118,11 @@ function DetailsCard() {
           </div>
         
         <PokeDimensions pokemon={pokemon}/>
+         </div>
+         
          
       </div>
-    </PokeBg>
+    
       
       
     );
--- src/Components/DetailsComponents/PokeTypes.jsx
+++ src/Components/DetailsComponents/PokeTypes.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const PokeTypes = ({ pokemon }) => {
+const PokeTypes = ({ pokemon,bgColor }) => {
   const [pokemonTypes, setPokemonTypes] = useState([]);
+  const [bgSecondColor, setSecondBgColor] = useState("");
 
   useEffect(() => {
     if (pokemon.types) {
@@ -11,17 +12,79 @@ const PokeTypes = ({ pokemon }) => {
         .then((response) => {
           const types = response.map((res) => res.data.name);
           setPokemonTypes(types);
+          const secondType = response[1]?.data?.name;
+          switch (secondType) {
+            case "fire":
+              setSecondBgColor("bg-[#F57D31]");
+              break;
+            case "poison":
+              setSecondBgColor("bg-[#7B61FF]");
+              break;
+            case "water":
+              setSecondBgColor("bg-[#6493EB]");
+              break;
+            case "grass":
+              setSecondBgColor("bg-[#74CB48]");
+              break;
+            case "electric":
+              setSecondBgColor("bg-[#F9CF30]");
+              break;
+            case "ground":
+              setSecondBgColor("bg-[#DEC16B]");
+              break;
+            case "fairy":
+              setSecondBgColor("bg-[#E69EAC]");
+              break;
+            case "fighting":
+              setSecondBgColor("bg-[#C12239]");
+              break;
+            case "psychic":
+              setSecondBgColor("bg-[#FB5584]");
+              break;
+            case "dragon":
+              setSecondBgColor("bg-[#7037FF]");
+              break;
+            case "ghost":
+              setSecondBgColor("bg-[#70559B]");
+              break;
+            case "ice":
+              setSecondBgColor("bg-[#9AD6DF]");
+              break;
+            case "bug":
+              setSecondBgColor("bg-[#A7B723]");
+              break;
+            case "rock":
+              setSecondBgColor("bg-[#B69E31]");
+              break;
+            case "steel":
+              setSecondBgColor("bg-[#B7B9D0]");
+              break;
+            case "dark":
+              setSecondBgColor("bg-[#75574C]");
+              break;
+            default:
+              setSecondBgColor("bg-[#AAA67F]");
+          }
         })
         .catch((error) => console.log(error));
     }
   }, [pokemon]);
+  
+  const gridColumns = pokemonTypes.length > 1 ? "grid-cols-2" : "grid-cols-1";
 
   return (
-    <div>
+    <div className="mx-auto text-center pt-10">
       {pokemonTypes.length !== 0 ? (
-        <div className="grid grid-cols-1 md:gap-x-2 md:w-auto">
-          {pokemonTypes.map((type) => (
-            <div key={type}>{type.charAt(0).toUpperCase()+type.slice(1)}</div>
+        <div className={`grid ${gridColumns} gap-x-4 md:grid-cols-1 md:gap-x-2 md:w-auto`}>
+          {pokemonTypes.map((type, index) => (
+            <div
+              key={type}
+              className={`text-center py-2 rounded-md text-white ${
+                index === 0 ? bgColor : bgSecondColor
+              }`}
+            >
+              {type.charAt(0).toUpperCase() + type.slice(1)}
+            </div>
           ))}
         </div>
       ) : (
